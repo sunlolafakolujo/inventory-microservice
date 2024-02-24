2cd4e3d1adc04ee7134c33b47b96020b09686270
sunlolafakolujo <dapafol@live.com>
2024-02-24 12:42:35 +0100

    update

---
+++ eureka-service-registry/src/main/java/com/inventory/eurekaserviceregistry/EurekaServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.eurekaserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
+	}
+
+}
