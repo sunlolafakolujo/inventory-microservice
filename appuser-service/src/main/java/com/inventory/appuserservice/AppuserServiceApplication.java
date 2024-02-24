cb4c52737a129cbc0a35bf073e6d83965acef99c
sunlolafakolujo <dapafol@live.com>
2024-02-24 12:29:08 +0100

    Update

--- appuser-service/src/main/java/com/inventory/appuserservice/AppuserServiceApplication.java
+++ appuser-service/src/main/java/com/inventory/appuserservice/AppuserServiceApplication.java
@@ -2,8 +2,10 @@ package com.inventory.appuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AppuserServiceApplication {
 
 	public static void main(String[] args) {
