ccd7f6532193479cb628200bd87645660a0617ab
sunlolafakolujo <dapafol@live.com>
2024-02-24 12:27:15 +0100

    Initial commit

---
+++ appuser-service/src/test/java/com/inventory/appuserservice/AppuserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.appuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppuserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
