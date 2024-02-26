3fa119eefde43954b707274b146f36a5894da75d
sunlolafakolujo <dapafol@live.com>
2024-02-26 22:26:30 +0100

    Error message class added

--- appuser-service/src/main/java/com/inventory/appuserservice/address/errormessage/ValidateErrorMessage.java
+++ appuser-service/src/main/java/com/inventory/appuserservice/address/errormessage/ValidateErrorMessage.java
@@ -1,2 +1,19 @@
-package com.inventory.appuserservice.address.errormessage;public class ValidateErrorMessage {
+package com.inventory.appuserservice.address.errormessage;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidateErrorMessage {
+    private HttpStatus httpStatus;
+    private String message;
+    private String description;
+    private Date date;
+
 }
