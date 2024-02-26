72d4ff7145b54a058efa7948637021bd464a6e98
sunlolafakolujo <dapafol@live.com>
2024-02-26 22:28:47 +0100

    Address web exception handler added

--- appuser-service/src/main/java/com/inventory/appuserservice/address/exception/AddressNotFoundExceptionRestHandler.java
+++ appuser-service/src/main/java/com/inventory/appuserservice/address/exception/AddressNotFoundExceptionRestHandler.java
@@ -1,2 +1,22 @@
-package com.inventory.appuserservice.address.exception;public class AddressNotFoundExceptionRestHandler {
+package com.inventory.appuserservice.address.exception;
+
+import com.inventory.appuserservice.address.errormessage.ValidateErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class AddressNotFoundExceptionRestHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(AddressNotFoundException.class)
+    public ResponseEntity<ValidateErrorMessage> addressNotFoundException(AddressNotFoundException anfe,
+                                                                         WebRequest webRequest){
+        ValidateErrorMessage vem= new ValidateErrorMessage(HttpStatus.NOT_FOUND, anfe.getMessage(),
+                webRequest.getDescription(false), new Date());
+        return new ResponseEntity<>(vem, HttpStatus.BAD_REQUEST);
+    }
 }
