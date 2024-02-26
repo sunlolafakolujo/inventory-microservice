1a610e3119aded63fc04a831d45a2219c14b2f90
sunlolafakolujo <dapafol@live.com>
2024-02-26 22:23:49 +0100

    Address update dto added

--- appuser-service/src/main/java/com/inventory/appuserservice/address/dto/AddressUpdate.java
+++ appuser-service/src/main/java/com/inventory/appuserservice/address/dto/AddressUpdate.java
@@ -1,2 +1,15 @@
-package com.inventory.appuserservice.address.dto;public class AddressUpdate {
+package com.inventory.appuserservice.address.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AddressUpdate {
+    private String id;
+    private String fullAddress;
+    private String city;
+    private String landmark;
+    private String state;
+    private String country;
 }
