82b080533b4c823859b248fce509ec5886aa0bf5
sunlolafakolujo <dapafol@live.com>
2024-02-26 22:21:52 +0100

    address request dto added

--- appuser-service/src/main/java/com/inventory/appuserservice/address/dto/AddressRequest.java
+++ appuser-service/src/main/java/com/inventory/appuserservice/address/dto/AddressRequest.java
@@ -1,2 +1,14 @@
-package com.inventory.appuserservice.address.dto;public class AddressRequest {
+package com.inventory.appuserservice.address.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AddressRequest {
+    private String fullAddress;
+    private String city;
+    private String landmark;
+    private String state;
+    private String country;
 }
