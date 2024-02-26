b7e36254c4b490ef14b9dcdbd150c92da901e20e
sunlolafakolujo <dapafol@live.com>
2024-02-26 22:25:19 +0100

    Address entity added

--- appuser-service/src/main/java/com/inventory/appuserservice/address/entity/Address.java
+++ appuser-service/src/main/java/com/inventory/appuserservice/address/entity/Address.java
@@ -1,2 +1,29 @@
-package com.inventory.appuserservice.address.entity;public class Address {
+package com.inventory.appuserservice.address.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE address SET deleted= true WHERE id=?")
+@Where(clause = "deleted = false")
+public class Address {
+
+    @Id
+    private String id;
+    private String fullAddress;
+    private String city;
+    private String landmark;
+
+    @Column(name = "states")
+    private String state;
+    private String country;
 }
