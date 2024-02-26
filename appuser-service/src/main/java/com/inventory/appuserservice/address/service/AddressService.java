ad786617830a48cc19e9ef43f7a5e0ef2dbcd437
sunlolafakolujo <dapafol@live.com>
2024-02-26 22:30:49 +0100

    Address service

--- appuser-service/src/main/java/com/inventory/appuserservice/address/service/AddressService.java
+++ appuser-service/src/main/java/com/inventory/appuserservice/address/service/AddressService.java
@@ -1,2 +1,14 @@
-package com.inventory.appuserservice.address.service;public interface AddressService {
+package com.inventory.appuserservice.address.service;
+
+import com.inventory.appuserservice.address.entity.Address;
+
+import java.util.List;
+
+public interface AddressService {
+    Address addAddress(Address address);
+    Address fetchAddressById(String id);
+    List<Address> fetchAllAddresses(int pageNumber, int pageSize);
+    Address editAddress(Address address, String id);
+    void deleteAddress(String id);
+    void deleteAllAddresses();
 }
